234492d776a27a1339a8c36817c62ec61ab6a950
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-02-07 12:18:24 -0500

    [+] Components: Header, Body, Footer

---
+++ src/app/components/footer.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `<h2>Pie de pagina</h2>`
+})
+export class FooterComponent {}
---
+++ src/app/components/header.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<h2>Encabezado</h2>`
+})
+export class HeaderComponent {}
